7c534838b076f2c62ea952efb7b6d1a4b63b433f
akshayghule128 <78530749+akshayghule128@users.noreply.github.com>
2024-07-20 19:59:08 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "jenkins-server-key" 
+    default     =  "Batch-24" 
 }
 
 variable "instance_type" { 
